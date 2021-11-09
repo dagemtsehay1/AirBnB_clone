a2b5faf94191e62a6688f71730399f0717bd6d42
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-11-09 11:38:22 +0300

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[k] = v
             
