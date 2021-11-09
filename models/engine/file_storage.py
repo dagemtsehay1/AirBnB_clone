9f5ea35d649df1d46e0c2ee8284ec37d83c27fa0
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-11-09 17:33:32 +0300

    adding save method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,3 +16,10 @@ class FileStorage:
         """Sets in __objects the obj with key <obj class name>.id."""
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
+        
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        with open(self.__file_path, mode='w') as f:
+            json.dump(d, f)
