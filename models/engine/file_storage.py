587db4ca2e766306123e42a43bf6d0c070c2f2a3
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-11-09 17:18:07 +0300

    adding new() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,3 +11,8 @@ class FileStorage:
     def all(self):
         """Returns the dictionary __objects"""
         return self.__objects
+    
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
