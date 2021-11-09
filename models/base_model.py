04dcf3676d3a01b0933c92ac54435800c6593d23
Dagem tsehay <dagemtsehay4@gmail.com>
2021-11-09 07:34:03 +0000

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains the prototype for BaseModel class."""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel of the AirBnB project."""
+    pass
