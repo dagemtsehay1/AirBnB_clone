bdbae82d65436d6d91865fd632e7b97322d65885
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-11-09 11:45:58 +0300

    updating the constructor

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "__class__":
+                    continue
                 else:
                     self.__dict__[k] = v
             
