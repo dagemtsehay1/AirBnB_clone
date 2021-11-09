071f154a5c10327ae6f66f4c92bc2d945579d0fa
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-11-09 11:14:47 +0300

    adding save method

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,15 @@ class BaseModel:
                     setattr(self, k, v)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
             
     def __str__(self):
         """Prints  [<class name>] (<self.id>) <self.__dict__>"""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
