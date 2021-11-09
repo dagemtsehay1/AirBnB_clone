73b6b2a4b36d9db26059664cd5651526fbe06790
Dagem tsehay <dagemtsehay4@gmail.com>
2021-11-09 07:46:49 +0000

    initializing basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+0;136;0c0;136;0c#!/usr/bin/python3
 """This module contains the prototype for BaseModel class."""
 from uuid import uuid4
 from datetime import datetime
@@ -6,4 +6,19 @@ from datetime import datetime
 
 class BaseModel:
     """BaseModel of the AirBnB project."""
-    pass
+    
+    def __init__(self, *args, **kwargs):
+        """Initializing the BaseModel"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
